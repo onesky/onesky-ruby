3c25091c1c43bcbadc90c36580ccefcf709e4d40
Victor Lam <victorebox@yahoo.com.hk>
2014-10-07 16:17:01 +0800

    init onesky-ruby

---
+++ lib/onesky/ruby.rb
@@ -0,0 +1,7 @@
+require "onesky/ruby/version"
+
+module Onesky
+  module Ruby
+    # Your code goes here...
+  end
+end
---
+++ lib/onesky/ruby/version.rb
@@ -0,0 +1,5 @@
+module Onesky
+  module Ruby
+    VERSION = "0.0.1"
+  end
+end
