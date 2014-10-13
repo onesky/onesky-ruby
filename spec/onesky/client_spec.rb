1d803a98cdc732e011ef4194ca3253d2f9ba45f4
Victor Lam <victorebox@yahoo.com.hk>
2014-10-13 16:35:03 +0800

    require spec_helper and refactor client

--- spec/onesky/client_spec.rb
+++ spec/onesky/client_spec.rb
@@ -1,10 +1,10 @@
-require 'onesky'
+require 'spec_helper'
 
 describe Onesky::Client do
 
-  it 'should able to set api_key, api_secret' do
-    client = Onesky::Client.new('api_key', 'api_secret')
+  let(:client) {Onesky::Client.new('api_key', 'api_secret')}
 
+  it 'should able to set api_key, api_secret' do
     expect(client.api_key).to eq 'api_key'
     expect(client.api_secret).to eq 'api_secret'
   end
