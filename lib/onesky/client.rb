92d2b3e452cc673ecd140a588b1b8d574891d9da
Victor Lam <victorebox@yahoo.com.hk>
2014-10-14 15:52:41 +0800

    remove test code

--- lib/onesky/client.rb
+++ lib/onesky/client.rb
@@ -13,10 +13,6 @@ module Onesky
     include Resources::Project
     include Helpers::Request
 
-    def self.test_print
-      p 'This is Onesky::Client.test_print()'
-    end
-
     attr_accessor :api_key, :api_secret, :project_id
 
     def initialize(key, secret)
