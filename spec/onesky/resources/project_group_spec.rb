2e2ba4695b236a22892594575cd8b82bda3588fa
Victor Lam <victorebox@yahoo.com.hk>
2014-10-14 11:46:01 +0800

    add spec for create project group

--- spec/onesky/resources/project_group_spec.rb
+++ spec/onesky/resources/project_group_spec.rb
@@ -2,7 +2,9 @@ require 'spec_helper'
 
 describe 'Project Group' do
 
-  let(:client) {Onesky::Client.new('api_key', 'api_secret')}
+  let(:api_key) {'api_key'}
+  let(:api_secret) {'api_secret'}
+  let(:client) {Onesky::Client.new(api_key, api_secret)}
 
   describe 'list_project_group' do
     it 'should list all available project group' do
@@ -18,6 +20,15 @@ describe 'Project Group' do
       stub_request(:get, 'https://staging-platform.api.onesky.io/1/project-groups/1')
         .to_return(body: {})
       response = client.show_project_group(1)
+      expect(response).to be_an_instance_of(Hash)
+    end
+  end
+
+  describe 'create_project_group' do
+    it 'should create a project group' do
+      stub_request(:post, full_path_with_auth_hash('/project-groups', api_key, api_secret))
+        .to_return(body: {})
+      response = client.create_project_group({name: 'New Project Group'})
       expect(response).to be_an_instance_of(Hash)
     end
   end
