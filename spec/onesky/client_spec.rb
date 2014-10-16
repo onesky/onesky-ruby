11032e5f832eee88fd7b0a84913c5e3d5fbe41f6
Victor Lam <victorebox@yahoo.com.hk>
2014-10-16 11:16:13 +0800

    add test for building auth_hash

--- spec/onesky/client_spec.rb
+++ spec/onesky/client_spec.rb
@@ -7,6 +7,7 @@ describe Onesky::Client do
   it 'should able to set api_key, api_secret' do
     expect(client.api_key).to eq 'api_key'
     expect(client.api_secret).to eq 'api_secret'
+    expect(client.auth_hash).to be_an_instance_of(Hash)
   end
 
 end
