aa7c7d2b1940f31905e85135e314bc386bb94f5b
Victor Lam <victorebox@yahoo.com.hk>
2014-12-31 12:01:20 +0800

    test required header

--- spec/onesky/helpers/request_spec.rb
+++ spec/onesky/helpers/request_spec.rb
@@ -7,10 +7,12 @@ describe 'Helpers::Request' do
   let(:client) {Onesky::Client.new(api_key, api_secret)}
   let(:project_id) {1}
   let(:project) {client.project(project_id)}
+  let(:http_header) {{'Content-Type' => 'application/json', 'Onesky-Plugin' => 'ruby-wrapper'}}
 
   describe '#get' do
     it 'should raise error for error response code' do
       stub_request(:get, full_path_with_auth_hash("/locales", api_key, api_secret))
+        .with(headers: http_header)
         .to_return(status: 400, body: {meta: {code: 400, message: 'error message'}}.to_json)
       expect {client.list_locale}.to raise_error(Onesky::Errors::BadRequestError, '400 Bad Request - error message')
     end
@@ -19,6 +21,7 @@ describe 'Helpers::Request' do
   describe '#post' do
     it 'should return response on error' do
       stub_request(:post, full_path_with_auth_hash("/project-groups", api_key, api_secret))
+        .with(headers: http_header)
         .to_return(status: 400, body: {meta: {code: 400, message: 'error message'}}.to_json)
       expect {client.create_project_group({name: 'TEST'})}.to raise_error(Onesky::Errors::BadRequestError, '400 Bad Request - error message')
     end
@@ -27,6 +30,7 @@ describe 'Helpers::Request' do
   describe '#post_multipart' do
     it 'should return response on error' do
       stub_request(:post, full_path_with_auth_hash("/projects/#{project_id}/files", api_key, api_secret))
+        .with(headers: {'Onesky-Plugin' => 'ruby-wrapper'})
         .to_return(status: 400, body: {meta: {code: 400, message: 'error message'}}.to_json)
       expect {project.upload_file({file: 'spec/fixture/en.yml', file_format: 'RUBY_YAML'})}.to raise_error(Onesky::Errors::BadRequestError, '400 Bad Request - error message')
     end
@@ -35,6 +39,7 @@ describe 'Helpers::Request' do
   describe '#put' do
     it 'should return response on error' do
       stub_request(:put, full_path_with_auth_hash("/projects/#{project_id}", api_key, api_secret))
+        .with(headers: http_header)
         .to_return(status: 400, body: {meta: {code: 400, message: 'error message'}}.to_json)
       expect {project.update({name: 'NEW NAME'})}.to raise_error(Onesky::Errors::BadRequestError, '400 Bad Request - error message')
     end
@@ -43,6 +48,7 @@ describe 'Helpers::Request' do
   describe '#delete' do
     it 'should return response on error' do
       stub_request(:delete, full_path_with_auth_hash("/projects/#{project_id}", api_key, api_secret))
+        .with(headers: http_header)
         .to_return(status: 400, body: {meta: {code: 400, message: 'error message'}}.to_json)
       expect {project.remove}.to raise_error(Onesky::Errors::BadRequestError, '400 Bad Request - error message')
     end
